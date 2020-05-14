7a59725852403fc9e104dd09b0bf5d51397401fb
Mostafa Samir <61014478+MostafaSamirAly@users.noreply.github.com>
2020-05-14 22:22:33 +0200

    fix api urls

--- MoviesProject/Models/Constants.swift
+++ MoviesProject/Models/Constants.swift
@@ -19,11 +19,11 @@ enum CoreDataEntities : String{
 
 enum ApiURL : String{
     
-    case mostPopular = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1c49378c151f43527b6b7af9330e8875"
+    case mostPopular = "https://api.themoviedb.org/3/movie/popular?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US&page=1"
     
-    case topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US"
+    case topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US&page=1"
     
-    case inTheatres = "https://api.themoviedb.org/3/movie/now_playing?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US"
+    case inTheatres = "https://api.themoviedb.org/3/movie/now_playing?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US&page=1"
     
    case search = "https://api.themoviedb.org/3/search/movie?api_key=1c49378c151f43527b6b7af9330e8875&language=en-US&query="
 }
